08337338246d95aa42e5f08e7d8feaadbfa15e6f
Thomas Dimperio <twdimperio@gmail.com>
2022-05-11 11:58:32 -0600

    add Ruby solution

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,10 @@
 def reverse_string(str)
-  # type your code in here
+  reversed = ""
+  str.chars.each do |char|
+    reversed = char + reversed
+  end
+
+  reversed
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +16,18 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
 
-  # Don't forget to add your own!
+  puts "Expecting: '!dlroW ,olleH'"
+  puts "=>", reverse_string("Hello, World!")
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+# Iterate through string, and add each new character to new string
+# plus the remainder of the reversed string... This way the reversed string 
+# gets the new chars added to the beginning, effectively reversing the order.
+
+
+# The reversed string starts as an empty string "", and on the first iteration,
+# the first char is added. Now the string has one char, the first char in the
+# original string. On subsequent iterations, the reversed string is added to
+# each new char in original order, thus pushing the first chars to the end and 
+# reversing the order of the string.
+# EX: "Hello" => "H" + ""; "ello" => "e" + "H"; "llo" => "l" + "eH" ... "olleH"
